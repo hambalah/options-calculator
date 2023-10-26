35ba809e7951968ced475f122dc39fe7e4dd4a46
Wei Xuan <wxseow.2021@scis.smu.edu.sg>
2023-10-26 17:11:36 +0800

    update black scholes cal
    - separated calculate function from class to run GUI
    - wrote 3 versions in the calculate function to divide variables, normal way, for-loop & list comprehension

--- black_scholes_GUI.py
+++ black_scholes_GUI.py
@@ -7,21 +7,18 @@ from scipy.stats import norm
 qtCreatorFile = "black_scholes_calc.ui" 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
-class Main(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
-
-        self.pushButton_Calculate.clicked.connect(self.calculate_option_price)
-        self.pushButton_Reset.clicked.connect(self.reset_fields)
-
-    def calculate_option_price(self):
-        S = float(self.lineEdit_SpotPrice.text())
-        K = float(self.lineEdit_StrikePrice.text())
-        T = float(self.lineEdit_TimetoExpiry.text())
-        r = float(self.lineEdit_InterestRate.text()) / 100
-        sigma = float(self.lineEdit_Volatility.text()) / 100
-        d = float(self.lineEdit_Dividend.text()) / 100
+def calculate_option_price(S, K, T, r, sigma, d):
+        r /= 100
+        sigma /= 100
+        d /= 100
+        
+        # for loop
+        # variables_to_adjust = [r, sigma, d]
+        # for variable in variables_to_adjust:
+        #     variable /= 100
+        
+        # list comprehension
+        # r, sigma, d = [var / 100 for var in (r, sigma, d)]
 
         d1 = (math.log(S / K) + (r - d + (sigma ** 2) / 2) * T) / (sigma * (T ** 0.5))
         d2 = d1 - sigma * (T ** 0.5)
@@ -29,8 +26,26 @@ class Main(QMainWindow, Ui_MainWindow):
         call_price = S * math.e**(-d * T) * norm.cdf(d1) - K * math.e**(-r * T) * norm.cdf(d2)
         put_price = K * math.e**(-r * T) * norm.cdf(-d2) - S * math.e**(-d * T) * norm.cdf(-d1)
 
-        self.lineEdit_CallPrice.setText(f"{call_price:.2f}")
-        self.lineEdit_CallPrice_2.setText(f"{put_price:.2f}")
+        return (round(call_price, 2), round(put_price, 2))
+        
+class Main(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.pushButton_Calculate.clicked.connect(self.Calculate)
+        self.pushButton_Reset.clicked.connect(self.reset_fields)
+
+    def Calculate(self):
+        S = float(self.lineEdit_SpotPrice.text())
+        K = float(self.lineEdit_StrikePrice.text())
+        T = float(self.lineEdit_TimetoExpiry.text())
+        r = float(self.lineEdit_InterestRate.text())
+        sigma = float(self.lineEdit_Volatility.text())
+        d = float(self.lineEdit_Dividend.text())
+        
+        call_price, put_price = calculate_option_price(S, K, T, r, sigma, d)
+        self.lineEdit_CallPrice.setText(str(call_price))
+        self.lineEdit_PutPrice.setText(str(put_price))
 
     def reset_fields(self):
         self.lineEdit_SpotPrice.clear()
@@ -40,7 +55,7 @@ class Main(QMainWindow, Ui_MainWindow):
         self.lineEdit_Volatility.clear()
         self.lineEdit_Dividend.clear()
         self.lineEdit_CallPrice.clear()
-        self.lineEdit_CallPrice_2.clear()
+        self.lineEdit_PutPrice.clear()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
